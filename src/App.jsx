b074568979e4e164e7ea2e120511ec3755afa95f
WilOkazaki <okazakisaito2012@gmail.com>
2023-03-31 18:47:25 -0400

    creacion del template del registro y login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { Route, Routes} from "react-router-dom";
 import "./App.css";
+import Login from "./components/Auth/Login";
+import Registro from "./components/Auth/Registro";
+import AuthTemplate from "./components/Auth/AuthTemplate";
 import { ContextProvider } from "./context/GlobalContext";
 
 function App() {
-  
 
   return (
     <ContextProvider>
       <Routes>
-        <Route path="/" element={<h1>Hola</h1>}>
+        <Route path="/" element={<AuthTemplate />}>
+          <Route path="login" element={<Login />} />
+          <Route path="registro" element={<Registro />} />
         </Route>
+
       </Routes>
     </ContextProvider>
   );
 }
 
 export default App;
+
