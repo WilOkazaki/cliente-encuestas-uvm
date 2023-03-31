cbd3a9d81341e2413d5e164d9804cab1366c75a5
WilOkazaki <okazakisaito2012@gmail.com>
2023-03-31 19:16:19 -0400

    pagina de error 404 y redireccion al login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Auth/Login";
 import Registro from "./components/Auth/Registro";
 import AuthTemplate from "./components/Auth/AuthTemplate";
 import { ContextProvider } from "./context/GlobalContext";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("/login");
+    }
+  }, [location]);
 
   return (
     <ContextProvider>
@@ -15,10 +23,17 @@ function App() {
           <Route path="registro" element={<Registro />} />
         </Route>
 
+        <Route
+          path="/*"
+          element={
+            <div className="w-100 vh-100 d-flex flex-column justify-content-center">
+              <h1 className="text-center">Error 404</h1>
+            </div>
+          }
+        />
       </Routes>
     </ContextProvider>
   );
 }
 
 export default App;
-
